64a57f32fb3002dfc71504e156741810df12bb72
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-03-29 13:46:25 -0700

    Write basic calculation functions

--- functions/calculations.ts
+++ functions/calculations.ts
@@ -28,3 +28,38 @@ export type Plane = {
   motor: Motor
 }
 
+export function calculateWingArea(plane: Plane) {
+  return plane.wing_span * plane.wing_chord
+}
+
+export function calculateMassOfPlane(plane: Plane) {
+  return 0.25 * calculateWingArea(plane)
+}
+
+export function calculateCoefficientOfLift(plane: Plane) {
+  return plane.coefficient_of_drag + (0.1 * Math.sqrt(plane.aspect_ratio))
+}
+
+export function calculateMassOfBattery(battery: Battery) {
+  return (battery.battery.voltage * battery.capacity) / 130
+}
+
+export function calculateMassOfPropeller(propeller: Propeller) {
+  return propeller.diameter
+}
+
+export function calculateMotorEfficiencyRating(plane: Plane) {
+  return 1.10 - 3.0E-2 * plane.propeller.diameter - 1.1E-4 * plane.motor.kV - 7.0E-6 * plane.propeller.diameter * plane.motor.kV
+}
+
+export function calculateAspectRatio(plane: Plane) {
+  return (plane.wing_span ** 2) / (plane.wing_span * plane.wing_chord)
+}
+
+export function calculateWCL(plane: Plane) {
+  return plane.mass / (calculateWingArea(plane) ** 1.5)
+}
+
+export function calculateWeight(plane: Plane) {
+  return plane.mass * 9.81
+}
